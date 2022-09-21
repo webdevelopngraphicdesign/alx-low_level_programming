20f191ce736efb4dc04670e1a8bce179af23f95e
Areo John <areo.john12345@gmail.com>
2022-09-21 10:48:52 -0700

    0x06. C - More pointers, arrays and strings task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - encode a string with rot13
+ * @p: the string
+ *
+ *
+ *
+ * Return: rot
+ */
+char *rot13(char *p)
+{
+	char *rot = p;
+	int j = 0;
+	char rotvalue1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotvalue2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*rot)
+	{
+		for (j = 0; rotvalue1[j]; j++)
+		{
+			if (*rot == rotvalue1[j])
+			{
+				*rot = rotvalue2[j];
+				break;
+			}
+		}
+		rot++;
+	}
+	return (p);
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * void print_buffer(char *b, int size);
+ *
