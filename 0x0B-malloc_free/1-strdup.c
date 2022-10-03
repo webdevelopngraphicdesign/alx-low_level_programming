7e3f20b9d65a720c24f16081cb9d6a8f39c13813
Areo John <areo.john12345@gmail.com>
2022-10-03 15:20:47 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,52 +1,35 @@
-#include "notrebloh.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * _strlen - return length of a string
+ * *_strdup - a function that duplicates a string
  *
- * @s: char type
- * Return:  length of string
- */
-int _strlen(char *s)
-{
-	int a;
-
-	for (a = 0; s[a] != '\0'; a++)
-	{
-
-	}
-	return (a);
-}
-
-/**
- * *_strdup - function to return pointer to string
- * @str: pointer to string array input
- * Return: pointer to string created
- */
+ * @str: input string to duplicate
+ *
+ * Return: NULL if str == NULL
+ *         @str
+*/
 
 char *_strdup(char *str)
 {
-	char *ptr;
-	int size;
-	int x;
+	int i = 0, l = 0;
+	char *s;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-	size = _strlen(str) + 1;
-	ptr = malloc(size * sizeof(char));
 
-	x = 0;
-	while (x < size)
+	/*calculate size of str*/
+	while (str[l] != '\0')
+		l++;
+
+	s = malloc((l + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
 	{
-		if (ptr == NULL)
-		{
-			return (NULL);
+		s[i] = str[i];
+		i++;
 	}
-		ptr[x] = str[x];
-		x++;
-	}
-	return (ptr);
+
+	return (s);
 }
